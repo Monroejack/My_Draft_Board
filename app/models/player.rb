c3595f555f5b6cff0378ba69fcece2fb1e4e608b
Monroejack <Monroejack2121@gmail.com>
2018-03-21 21:59:43 -0400

    Minor change

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,9 @@
 class Player < ApplicationRecord
+  require 'csv'
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      Player.create! row.to_hash
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# Player.destroy_all
 #
-# Examples:
+# require 'csv'
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# csv_text = File.read(Rails.root.join('lib', 'seeds', 'players.csv'))
+# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+# csv.each do |row|
+#   puts row.to_hash
+# end
+
+
+#   t = Transaction.new
+#   Players = {}
+#   t.last_name = row['last_name']
+#   t.first_name = row['first_name']
+#   t.school = row['school']
+#   t.position = row['position']
+#   t.cbs_projection = row['cbs_projection']
+#   t.nfl_projection = row['nfl_projection']
+#   t.drafttek_projection = row['drafttek_projection']
+#   t.composite_rating = row['composite_rating']
+#   t.year = row['year']
+#   t.save
+#   t.all >> Players
+#   puts "#{t.position} #{t.first_name} #{t.last_name} from #{school} saved"
+# end
