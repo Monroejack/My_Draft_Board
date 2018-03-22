41e04030feb838634b7bd7d9f2a3c73f652cbd03
Monroejack <Monroejack2121@gmail.com>
2018-03-22 15:53:47 -0400

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,16 @@
 # Player.destroy_all
-#
-# require 'csv'
-#
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'players.csv'))
-# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-# csv.each do |row|
-#   puts row.to_hash
-# end
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'players.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+# puts csv
+csv.each do |row|
+  # row is a CSV::Row object, #to_h turns it into a Hash, which ActiveRecord will
+  # happily use as attributes for the creation of a new record.
+  Player.create(row.to_h)
+end
+# puts Player.all
 
 
 #   t = Transaction.new
